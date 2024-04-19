ec62dda634880cabf72af6ad9db42c544772d00f
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2024-04-18 20:47:37 -0500

    add uploadthing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
@@ -11,8 +10,8 @@ export default async function HomePage() {
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
             <img src={image.url} />
             <div>{image.name}</div>
           </div>
