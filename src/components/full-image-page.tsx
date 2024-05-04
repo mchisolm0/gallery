265573410c6cf254fa558589a8fe126ebbcb911f
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2024-05-04 11:18:45 -0500

    create better full page img

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,8 +1,30 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const idAsNumber = Number(props.id);
 
   const image = await getImage(idAsNumber);
-  return <img src={image.url} className="w-96" alt={image.name} />;
+
+  const userInfo = await clerkClient.users.getUser(image.userId);
+
+  return (
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+      <div className="flex-shrink flex-grow">
+        <img src={image.url} className="object-contain" alt={image.name} />
+      </div>
+      <div className="boarder-1 flex h-full w-56 flex-shrink-0 flex-col">
+        <div className="boarder-b p-2 text-xl">{image.name}</div>
+        <div className="p-2">
+          <div>Uploaded By:</div>
+          <div>{userInfo.fullName}</div>
+        </div>
+
+        <div className="p-2">
+          <div>Created On:</div>
+          <div>{image.createdAt.toLocaleDateString()}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
