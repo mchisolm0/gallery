9cd525b8978c37cdd6c768b751012caea59175f9
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2024-05-04 11:45:53 -0500

    update text before signin and upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,31 +8,39 @@ export const dynamic = "force-dynamic";
 async function Images() {
   const images = await getMyImages();
 
-  return (
-    <div className="flex flex-wrap justify-center gap-4 p-4">
-      {images.map((image) => (
-        <div key={image.id} className="flex h-48 w-48 flex-col">
-          <Link href={`/img/${image.id}`}>
-            <Image
-              src={image.url}
-              alt={image.name}
-              style={{ objectFit: "contain" }}
-              width={192}
-              height={192}
-            />
-          </Link>
-          <div>{image.name}</div>
-        </div>
-      ))}
-    </div>
-  );
+  if (images.length === 0) {
+    return (
+      <div className="h-full w-full p-4 text-center text-2xl">
+        Use the upload button above to begin.
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-wrap justify-center gap-4 p-4">
+        {images.map((image) => (
+          <div key={image.id} className="flex h-48 w-48 flex-col">
+            <Link href={`/img/${image.id}`}>
+              <Image
+                src={image.url}
+                alt={image.name}
+                style={{ objectFit: "contain" }}
+                width={192}
+                height={192}
+              />
+            </Link>
+            <div>{image.name}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default async function HomePage() {
   return (
     <main className="">
       <SignedOut>
-        <div className="h-full w-full text-center text-2xl">
+        <div className="h-full w-full p-4 text-center text-2xl">
           Please sign in above.
         </div>
       </SignedOut>
